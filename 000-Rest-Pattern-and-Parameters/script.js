de6707f91066bd8add970723811e9b4494e05e03
buivantan01 <buivantan1234566@gmail.com>
2022-01-18 08:34:54 +0700

    Updated lesson

---
+++ 000-Rest-Pattern-and-Parameters/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+ const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+ const opening Hours = {
+
+ [weekdays [3]]: {
+open: 12,
+
+close: 22,
+
+ },
+
+
+
+[weekdays [4]]: {
+
+
+
+open: 11,
+
+
+
+close: 23,
+
+ },
+
+
+};
+
+
+const restaurant = {
+
+name: 'Classico Italiano',
+
+location: 'Via Angelo Tavanti 23, Firenze, Italy', categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+
+starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'1,
+
+mainMenu: ['Pizza', 'Pasta', 'Risotto'],
